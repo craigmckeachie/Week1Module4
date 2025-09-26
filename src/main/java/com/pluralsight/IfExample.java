87d482d068295f68288a50157a057d6a8a777acf
Craig McKeachie <craigdmckeachie@gmail.com>
2025-09-26 16:54:46 -0400

    if example

---
+++ src/main/java/com/pluralsight/IfExample.java
@@ -0,0 +1,26 @@
+package com.pluralsight;
+
+public class IfExample {
+
+    public static void main(String[] args) {
+
+        int age = 76;
+
+
+        if ( age>= 21 && age <=75 ){
+            System.out.println("You can enter the bar.");
+        }else if(age >= 75){
+            System.out.println("Go home grandpa!");
+        }
+        else{
+            System.out.println("Go home punk.");
+        }
+    }
+
+
+
+
+
+
+
+}
