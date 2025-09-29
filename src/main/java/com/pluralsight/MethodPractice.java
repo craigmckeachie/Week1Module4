683e8bc0c02a9fedf22aa4798758ac7c2d5df6a3
Craig McKeachie <craigdmckeachie@gmail.com>
2025-09-29 16:08:09 -0400

    refactored fish to return the caught fish, created and called add method

--- src/main/java/com/pluralsight/MethodPractice.java
+++ src/main/java/com/pluralsight/MethodPractice.java
@@ -4,27 +4,56 @@ public class MethodPractice {
 
 
     public static void main(String[] args) {
-        fish();
-        dance();
-        sayHi();
+        String type = "Salsa";
+        dance(type, 55);
+        dance("Hip Hop", 77);
+        dance("Break Dance", 22);
+        dance("Tap", 43);
+
+        sayHi("Mario");
+        sayHi("Jada");
+        sayHi("Malika");
+
+
+        String whatICaught = fish();
+        System.out.println(whatICaught);
+//        OR
+//        System.out.println(fish());
+
+
+        int result = add(2,5);
+        System.out.println(result);
+
     }
 
-    public static void sayHi(){
-        System.out.println("Hi");
+    public static int add(int a, int b){
+        int sum = a + b;
+        return sum;
     }
 
-    public static void dance(){
-        System.out.println("WoahhhhhOhhhhhh");
-        System.out.println("WoahhhhhOhhhhhh");
-        System.out.println("Stop!");
-        System.out.println("Hammer time!");
+
+
+
+    public static String fish(){
+//        System.out.println("Get out fishing rod");
+//        System.out.println("Bait the hook");
+//        System.out.println("Cast");
+//        System.out.println("Wait....forever");
+        return "Humungous Catfish";
     }
 
-    public static void fish(){
-        System.out.println("Get out fishing rod");
-        System.out.println("Bait the hook");
-        System.out.println("Cast");
-        System.out.println("Wait....forever");
+    public static void dance(String typeOfDancing, int magicNumber){
+        System.out.printf("I am %s dancing", typeOfDancing);
+        System.out.println("Your magic number is:" + magicNumber);
+        System.out.println();
     }
+
+    public static void sayHi(String firstName){
+        System.out.println("Hi " + firstName);
+    }
+
+
+
+
 
 }
