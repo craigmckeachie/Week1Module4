d7fdcefd1a3fdc8aacc0f332c2639e2aabfecd93
Craig McKeachie <craigdmckeachie@gmail.com>
2025-09-29 13:39:30 -0400

    method practice

---
+++ src/main/java/com/pluralsight/MethodPractice.java
@@ -0,0 +1,30 @@
+package com.pluralsight;
+
+public class MethodPractice {
+
+
+    public static void main(String[] args) {
+        fish();
+        dance();
+        sayHi();
+    }
+
+    public static void sayHi(){
+        System.out.println("Hi");
+    }
+
+    public static void dance(){
+        System.out.println("WoahhhhhOhhhhhh");
+        System.out.println("WoahhhhhOhhhhhh");
+        System.out.println("Stop!");
+        System.out.println("Hammer time!");
+    }
+
+    public static void fish(){
+        System.out.println("Get out fishing rod");
+        System.out.println("Bait the hook");
+        System.out.println("Cast");
+        System.out.println("Wait....forever");
+    }
+
+}
